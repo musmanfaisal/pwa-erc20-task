c08bcd9b183484b0f9d2b226815c3f2c9a2e9590
usmanfaisal60 <usman.faisal60@gmail.com>
2022-04-09 23:40:48 +0500

    .env file added in git ignore

--- src/services/utils/loader-hook.ts
+++ src/services/utils/loader-hook.ts
@@ -12,10 +12,10 @@ export const useLoader = (initialState: boolean = false) => {
             if (turnLoaderOn) on();
             // api has to be a fucntion returning axios promise i.e () => axios.get("http://example.com")
             const { data } = await api();
-            if (process.env.REACT_APP_MODE === "local") console.log(data);
+            if (import.meta.env.VITE_APP_MODE === "local") console.log(data);
             callBack(data);
         } catch (e) {
-            if (process.env.REACT_APP_MODE === "local") console.log(e);
+            if (import.meta.env.VITE_APP_MODE === "local") console.log(e);
             errorCallback && errorCallback(e);
         } finally {
             off();
